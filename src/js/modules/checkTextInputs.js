8a3e1366eefccd7ccbf0e5538da9d0e5a00572aa
Ramil <vk2011@bk.ru>
2021-07-25 10:03:41 +0500

    add mask phone

--- src/js/modules/checkTextInputs.js
+++ src/js/modules/checkTextInputs.js
@@ -2,17 +2,17 @@ const checkTextInputs = (selector) => {
 
     const txtInputs = document.querySelectorAll(selector);
 
-    txtInputs.forEach(input => {
-        input.addEventListener('keypress', function(e) {
-            if (e.key.match(/[^а-яё 0-9]/ig)) {
-                e.preventDefault();
+    txtInputs.forEach(input => { /* Перебираем каждый инпут */
+        input.addEventListener('keypress', function(e) { /* И вешаем обработчик события нажатия клавиши  */
+            if (e.key.match(/[^а-яё 0-9]/ig)) { /* Если значение клавиши русские буквы и цифры */
+                e.preventDefault(); /* То сбрасываем стандартные настройки */
             }
         });
 
-        input.addEventListener('input', (e) => {
-			if (input.value.match(/[a-z]/ig)) {
-				input.value = '';
-                e.preventDefault();
+        input.addEventListener('input', (e) => { /* При событии инпут */
+			if (input.value.match(/[a-z]/ig)) { /* Проверяем на английские буквы */
+				input.value = ''; /* Если они есть то подставляем пустое значение */
+                e.preventDefault(); /* И сбрасываем стандартные настройки */
 			}
 		});
     });
--- src/js/modules/mask.js
+++ src/js/modules/mask.js
@@ -1,51 +1,54 @@
 const mask = (selector) => {
 
+    /* Установка позиции курсора */
     let setCursorPosition = (pos, elem) => {
 
         elem.addEventListener('click', () => {
-			elem.selectionStart = elem.selectionEnd = elem.value.length;
+			elem.selectionStart = elem.selectionEnd = elem.value.length; /* если кликаем на начало или конец, то будем будет присваивать значение количества элементов */
 		});
 
-        elem.focus();
+        elem.focus(); /* Установили фокус на элементе */
 
-        if (elem.setSelectionRange) {
-            elem.setSelectionRange(pos, pos);
-        } else if (elem.createTextRange) {
-            let range = elem.createTextRange();
+        if (elem.setSelectionRange) { /* Если у элемента есть такой метод */
+            elem.setSelectionRange(pos, pos); /* То устанавливаем у этого метода начальную и конечную позицию (если один и тот же элемент, то курсор будет в определенной позиции) */
+        } else if (elem.createTextRange) { /* Если у элемента есть такой метод (для IE) */
+            let range = elem.createTextRange(); /* То используем такой метод */
 
-            range.collapse(true);
-            range.moveEnd('character', pos);
-            range.moveStart('character', pos);
-            range.select();
+            range.collapse(true); /* Устанавливаем граничные точки диапазона в true */
+            range.moveEnd('character', pos); /* Конечная точка */
+            range.moveStart('character', pos); /* Начальная точка */
+            range.select(); /* Установили курсор и выделили элемент */
         }
     };
 
     function createMask(e) {
-        let matrix = '+7 (___) ___ __ __',
+        let matrix = '+7 (___) ___ __ __', /* Маска */
             i = 0,
-            def = matrix.replace(/\D/g, ''),
-            val = this.value.replace(/\D/g, '');
+            def = matrix.replace(/\D/g, ''), /* Все не цифры заменяем на пустую строку */
+            val = this.value.replace(/\D/g, ''); /* У контекста (на основе того, что ввел пользователь) все не цифры заменяем на пустую строку */
 
-        if (def.length >= val.length) {
-            val = def;
+        if (def.length >= val.length) { /* Если количество цифр в def будет больше или равно в val */
+            val = def; /* То мы подставляем дефолтное значение def */
         }
 
-        this.value = matrix.replace(/./g, function(a) {
-            return  /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+        this.value = matrix.replace(/./g, function(a) { /* В контекст (куда ввел пользователь) кладем матрицу со всеми элеменатми и для каждого элемента создаем функцию */
+            return  /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a; 
+            /* возвращаем каждый символ, проверяем входит ли он в определенный диапазон (цифры) и i будет меньше количеству символов в val (которое уже избавилось от всех не цифр). */
+            /* Если условие выполнилось, то будем из функции возвращать следующий символ. А если i будет больше или равно количеству символов в val, то будем возвращать пустую строку. Если нет, то будем возвращать a */
         });
 
-        if (e.type === 'blur') {
-            if (this.value.length == 2) {
-                this.value = '';
+        if (e.type === 'blur') { /* Если тип события блюр */
+            if (this.value.length == 2) { /* Если количество символов будет равно 2 */
+                this.value = ''; /* То записываем пустую строку */
             }
         } else {
-            setCursorPosition(this.value.length, this);
+            setCursorPosition(this.value.length, this); /* Если другое событие, то передаем функцию с аргументами (количество символов, которое есть сейчас и ссылка на элемент, который сейчас в работе) */
         }
     }
 
-    let inputs = document.querySelectorAll(selector);
+    let inputs = document.querySelectorAll(selector); /* Создаем переменную инпутс */
 
-    inputs.forEach(input => {
+    inputs.forEach(input => { /* На каждый инпут вешаем несколько обработчиков событий */
         input.addEventListener('input', createMask);
         input.addEventListener('focus', createMask);
         input.addEventListener('blur', createMask);
