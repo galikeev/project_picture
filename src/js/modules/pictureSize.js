e69a0846a76318bbf8327780cb3ef60d645ad12a
Ramil <vk2011@bk.ru>
2021-07-27 17:52:40 +0500

    change css style

--- src/js/modules/pictureSize.js
+++ src/js/modules/pictureSize.js
@@ -1,7 +1,7 @@
 const pictureSize = (imgSelector) => {
 
     const blocks = document.querySelectorAll(imgSelector);
-
+    /* Функция по показу изображения */
     function showImg (block) {
         const img = block.querySelector('img');
         img.src = img.src.slice(0, -4) + '-1.png'; /* Вырезаем в пути картинки последние 4 символа и приписываем -1.png */
@@ -9,7 +9,7 @@ const pictureSize = (imgSelector) => {
             p.style.display = 'none'; /* скрываем */
         });
     }
-
+    /* Функция по скрытию изображения */
     function hideImg (block) {
         const img = block.querySelector('img');
         img.src = img.src.slice(0, -6) + '.png'; /* Вырезаем в пути картинки последние 6 символов и приписываем .png */
@@ -17,12 +17,12 @@ const pictureSize = (imgSelector) => {
             p.style.display = 'block'; /* показываем */
         });
     }
-
+    /* Перебираем все блоки и каждому блоку вешаем события */
     blocks.forEach(block => {
-        block.addEventListener('mouseover', () => { /* Когда над элементом */
+        block.addEventListener('mouseover', () => { /* Когда курсор над элементом, то показываем иозображение */
             showImg(block);
         });
-        block.addEventListener('mouseout', () => { /* Когда уходит с элемента */
+        block.addEventListener('mouseout', () => { /* Когда курсор уходит с элемента, то скрываем изображение */
             hideImg(block);
         });
     });
