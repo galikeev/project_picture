2d7d84c60b3da151e6652c75a193cf83340aef08
Ramil <vk2011@bk.ru>
2021-07-27 17:15:00 +0500

    add chngeImg

--- src/js/modules/pictureSize.js
+++ src/js/modules/pictureSize.js
@@ -4,25 +4,25 @@ const pictureSize = (imgSelector) => {
 
     function showImg (block) {
         const img = block.querySelector('img');
-        img.src = img.src.slice(0, -4) + '-1.png';
-        block.querySelectorAll('p:not(.sizes-hit)').forEach(p => {
-            p.style.display = 'none';
+        img.src = img.src.slice(0, -4) + '-1.png'; /* Вырезаем в пути картинки последние 4 символа и приписываем -1.png */
+        block.querySelectorAll('p:not(.sizes-hit)').forEach(p => { /* все параграфы в блоке, кроме с классом .sizes-hit */
+            p.style.display = 'none'; /* скрываем */
         });
     }
 
     function hideImg (block) {
         const img = block.querySelector('img');
-        img.src = img.src.slice(0, -6) + '.png';
-        block.querySelectorAll('p:not(.sizes-hit)').forEach(p => {
-            p.style.display = 'block';
+        img.src = img.src.slice(0, -6) + '.png'; /* Вырезаем в пути картинки последние 6 символов и приписываем .png */
+        block.querySelectorAll('p:not(.sizes-hit)').forEach(p => { /* все параграфы в блоке, кроме с классом .sizes-hit */
+            p.style.display = 'block'; /* показываем */
         });
     }
 
     blocks.forEach(block => {
-        block.addEventListener('mouseover', () => {
+        block.addEventListener('mouseover', () => { /* Когда над элементом */
             showImg(block);
         });
-        block.addEventListener('mouseout', () => {
+        block.addEventListener('mouseout', () => { /* Когда уходит с элемента */
             hideImg(block);
         });
     });
