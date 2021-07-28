073371514a7ceb50fc8df78426742217cd5f4ddc
Ramil <vk2011@bk.ru>
2021-07-28 19:14:56 +0500

    add scroll animation

--- src/js/modules/scrolling.js
+++ src/js/modules/scrolling.js
@@ -1,40 +1,39 @@
 const scrolling = (upSelector) => {
 
-    const upElem = document.querySelector(upSelector);
+    const upElem = document.querySelector(upSelector); /* получаем кнопку вверх */
 
     window.addEventListener('scroll', () => {
-        if (document.documentElement.scrollTop > 1650) {
-            upElem.classList.add('animated', 'fadeIn');
+        if (document.documentElement.scrollTop > 1650) { /* если прокрученное расстояние больше 1650 пикселей */
+            upElem.classList.add('animated', 'fadeIn'); /* то показываем кнопку вверх с анимацией */
             upElem.classList.remove('fadeOut');
         } else {
-            upElem.classList.add('fadeOut');
-            upElem.classList.remove('fadeIn');
+            upElem.classList.add('fadeOut'); /* скрываем кнопку вверх с анимацией */
+            upElem.classList.remove('fadeIn'); 
         }
     });
 
-
-    // Скролл с помощью requestAnimationFrame
-    let links = document.querySelectorAll('[href^="#"]'),
-        speed = 0.2;
+    /* Скролл с помощью requestAnimationFrame */
+    let links = document.querySelectorAll('[href^="#"]'), /* получаем все ссылки, которые начинаются с # (хэш) */
+        speed = 0.2; /* переменная отвечает за скорость */
     
     links.forEach(item => {
         item.addEventListener('click', function(event) {
             event.preventDefault();
 
-            let heightTop = document.documentElement.scrollTop,
-                hash = this.hash,
-                toBlock = document.querySelector(hash).getBoundingClientRect().top,
-                start = null;
+            let heightTop = document.documentElement.scrollTop, /* прокрученное расстояние */
+                hash = this.hash, /* текущий хэш (#) */
+                toBlock = document.querySelector(hash).getBoundingClientRect().top, /* верхняя граница  */
+                start = null; /* стартовая позиция */
 
             requestAnimationFrame(step);
 
-            function step(time) {
+            function step(time) { /* аргумент будет передаваться автоматически */
                 if (start === null) {
                     start = time;
                 }
 
                 let progress = time - start,
-                    r = (toBlock < 0 ? Math.max(heightTop - progress/speed, heightTop + toBlock) : Math.min(heightTop + progress/speed, heightTop + toBlock));
+                    r = (toBlock < 0 ? Math.max(heightTop - progress/speed, heightTop + toBlock) : Math.min(heightTop + progress/speed, heightTop + toBlock)); /* отвечает за количество пикселей, которые необходимо пролистать в течение анимации */
                 document.documentElement.scrollTo(0, r);
 
                 if (r != heightTop + toBlock) {
@@ -53,15 +52,15 @@ const scrolling = (upSelector) => {
 
     // const calcScroll = () => {
     //     upElem.addEventListener('click', function(event) {
-    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
+    //         let scrollTop = Math.round(body.scrollTop || element.scrollTop); /* Узнаем какое расстояние было пролистано вниз*/
 
-    //         if (this.hash !== '') {
-    //             event.preventDefault();
+    //         if (this.hash !== '') { /* Если текущий хэш(#) не пустой */
+    //             event.preventDefault(); /* То отменяем стандартное поведение браузера */
     //             let hashElement = document.querySelector(this.hash),
-    //                 hashElementTop = 0;
+    //                 hashElementTop = 0; /* Родительский элемент */
 
-    //             while (hashElement.offsetParent) {
-    //                 hashElementTop += hashElement.offsetTOp;
+    //             while (hashElement.offsetParent) { 
+    //                 hashElementTop += hashElement.offsetTop;
     //                 hashElement = hashElement.offsetParent;
     //             }
 
@@ -71,14 +70,14 @@ const scrolling = (upSelector) => {
     //     });
     // };
 
-    // const smoothScroll = (from, to, hash) => {
-    //     let timeInterval = 1,
-    //         prevScrollTop,
-    //         speed;
+    // const smoothScroll = (from, to, hash) => { /* аргументы (откуда, куда и текущий хэш) */
+    //     let timeInterval = 1, /* время, через которое будет производиться анимация */
+    //         prevScrollTop, /* предшествующее значение */
+    //         speed; /* с какой скоростью будет происходить анимация */
 
-    //     if (to > from) {
-    //         speed = 30;
-    //     } else {
+    //     if (to > from) { /* если куда больше, чем откуда (то есть сверху вниз) */
+    //         speed = 30; 
+    //     } else { /* если снизу вверх */
     //         speed = -30;
     //     }
 
